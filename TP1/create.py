0ad20cad29011201013834292ac9083e7f12c19b
victoria rossi <vrossi@itba.edu.ar>
2023-03-19 23:35:47 -0300

    newone

--- TP1/create.py
+++ TP1/create.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import arcade
 import random
 
@@ -22,6 +23,9 @@ python -m arcade.examples.array_backed_grid_sprites_2
 ROW_COUNT = 16
 COLUMN_COUNT = 15
 
+FIRST_ROW = 14
+FIRST_COLUMN = 0
+
 # This sets the WIDTH and HEIGHT of each grid location
 WIDTH = 30 
 HEIGHT = 30
@@ -35,20 +39,20 @@ SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 SCREEN_TITLE = "Array Backed Grid Buffered Example"
 
-# WHITE = (255, 255, 255)
-# RED = (255, 0, 0)
-# GREEN = (0, 255, 0)
-# BLUE = (0, 0, 255)
-# PINK = (255, 200, 200)
-# YELLOW = (255, 255, 0)
+
+
+
+
+
+
 
 colors = {
-    0: arcade.color.WHITE,
-    1: arcade.color.RED,
-    2: arcade.color.BLUE,
-    3: arcade.color.GREEN,
-    4 : arcade.color.PINK,
-    5 :arcade.color.YELLOW
+    0: arcade.color.WHITE, # WHITE = (255, 255, 255)
+    1: arcade.color.RED, # RED = (255, 0, 0)
+    2: arcade.color.BLUE, # BLUE = (0, 0, 255)
+    3: arcade.color.GREEN, # GREEN = (0, 255, 0)
+    4: arcade.color.PINK, # PINK = (255, 192, 203)
+    5: arcade.color.YELLOW # YELLOW = (255, 255, 0)
 }
 
 class MyGame(arcade.Window):
@@ -134,17 +138,43 @@ def fill_zone(self, x, y, selected_color):
             # Simply return from this method since nothing needs updating
             return
 
-        first_color = self.grid_sprites[ROW_COUNT - 2][0]
-        print(first_color)
-        it_fill_zone(self, 14,0, first_color, selected_color)
+        first_color = self.grid_sprites[FIRST_ROW][FIRST_COLUMN]
+        # print([self.grid_sprites[FIRST_ROW][i].color for i in range(len(self.grid_sprites[FIRST_ROW]))])
+        # print("First color: ",first_color.color)
+        pos = FIRST_ROW * COLUMN_COUNT + FIRST_COLUMN
+        print("First color: ",self.grid_sprite_list[pos].color)
+        it_fill_zone(self, FIRST_ROW, FIRST_COLUMN, first_color, selected_color)
+
 def it_fill_zone(self, row,column, first_color, selected_color):
+    print(row, column)
+    print("Before:\t Actual:",self.grid_sprites[row][column].color, " First: ",first_color.color) #hay un problema aca que toma el color actual siempre igual a first color
+    
+    if row >= ROW_COUNT-1 or column >= COLUMN_COUNT:
+        return
+
+    if row < 0 or column < 0:
+        return
+    
+    if self.grid_sprites[row][column] != first_color:
+        return
+    
+    it_fill_zone(self, row-1, column, first_color, selected_color)
+    it_fill_zone(self, row+1, column, first_color, selected_color)
+    it_fill_zone(self, row, column-1, first_color, selected_color)
+    it_fill_zone(self, row, column+1, first_color, selected_color) 
+
+    print("Selected: ", selected_color)
     if self.grid_sprites[row][column] == first_color:
-        print('son iguales')
         self.grid_sprites[row][column].color = selected_color
-    # it_fill_zone(self, row-1, col, first_color, selected_color)
-    # it_fill_zone(self, row+1, col, first_color, selected_color)
-    # it_fill_zone(self, row, col-1, first_color, selected_color)
-    # it_fill_zone(self, row, col+1, first_color, selected_color)
+        x = column * (WIDTH + MARGIN) + (WIDTH / 2 + MARGIN)
+        # y = row  * (HEIGHT + MARGIN) + (HEIGHT / 2 + MARGIN)
+        # sprite = arcade.SpriteSolidColor(WIDTH, HEIGHT, arcade.color.RED)
+        # sprite.center_x = x
+        # sprite.center_y = y
+        # self.grid_sprites[row][column].color = sprite.color
+    print("After: ",self.grid_sprites[row][column].color)
+    
+
 
 
 def main():
