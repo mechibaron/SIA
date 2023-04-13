9d35c8aa2c6b62492d6c018a878c7ddbd20a5f43
Agustina Ortu <aortu@itba.edu.ar>
2023-04-13 10:55:48 -0300

    Completing output showing colors

---
+++ TP2/genetic_boltz.py
@@ -0,0 +1,203 @@
+from select import select
+import numpy as np
+from colors import distance, MAX_DISTANCIA
+from enum import Enum
+
+rng = np.random.default_rng()
+
+def aptitud(color, goal):
+  return 1 - (distance(color, goal) / MAX_DISTANCIA)
+
+def pseudo_aptitud(k, goal):
+  return (k-goal)/k
+
+
+# def pseudo_aptitud(rank, k):
+#   return (k-rank)/k
+# SELECCION
+
+def select_elite(pop, mixes, f, k, goal,i):
+  fitness = np.apply_along_axis(f, 1, mixes, (goal))
+  order = np.argsort(fitness)
+  best = np.flip(pop[order], axis=0)
+
+  return best[:k]
+
+def select_roulette(pop, mixes, f, k, goal,i):
+  fitness = np.apply_along_axis(f, 1, mixes, (goal))
+  sum_fitness = np.sum(fitness)
+
+  ps = fitness / sum_fitness
+  qs = np.cumsum(ps)
+  rs = rng.uniform(0., 1., size=(k,))
+
+  selection = []
+  for ri in rs:
+    for i in range(len(qs)):
+      if (qs[i-1] < ri <= qs[i]):
+        selection.append(pop[i])
+
+  return np.array(selection)
+
+def select_tourney(pop, mixes, f, k, goal, m=2):
+  fitness = np.apply_along_axis(f, 1, mixes, (goal))
+  order = np.argsort(fitness)
+  pop = pop[order]
+
+  selection = []
+  for _ in range(k):
+    idxs = rng.choice(len(pop), size=k, replace=False)
+    winner = pop[np.max(idxs)]
+    selection.append(winner)
+
+  return np.array(selection)
+
+def select_roulette_ranking(pop,pseudo_fitness_list, k, i):
+  sum_pseudo_fitness = np.sum(pseudo_fitness_list)
+  ps = pseudo_fitness_list / sum_pseudo_fitness
+  qs = np.cumsum(ps)
+  rs = rng.uniform(0., 1., size=(k,))
+
+  selection = []
+  for ri in rs:
+    for i in range(len(qs)):
+      if (qs[i-1] < ri <= qs[i]):
+        selection.append(pop[i])
+
+  return np.array(selection)
+
+# def select_ranking(pop, mixes, f, k, goal, i):
+#   fitness = np.apply_along_axis(f, 1, mixes, (goal))
+#   rank=np.argsort(fitness)[::-1]
+#   #rank=sorted(fitness, reverse=True)
+#   #pseudo_fitness=np.zeros_like(fitness)
+  
+#   for i in range(k):
+#     fitness[i] = (k-(rank[i]+1))/k
+
+#   sum_fitness = np.sum(fitness)
+#   ps = fitness / sum_fitness
+#   qs = np.cumsum(ps)
+#   rs = rng.uniform(0., 1., size=(k,))
+
+#   selection = []
+#   for ri in rs:
+#     for i in range(len(qs)):
+#       if (qs[i-1] < ri <= qs[i]):
+#         selection.append(pop[i])
+
+#   return np.array(selection)
+
+#   # # order = np.argsort(fitness)
+#   # # pop = pop[order]
+#   # # Utilizar la ruleta para seleccionar un individuo de la población
+#   # final_roulette = select_roulette_ranking(pop,fitness, k)
+
+#   # return final_roulette
+
+def select_ranking(pop, mixes, f, k, goal):
+  fitness = np.apply_along_axis(f, 1, mixes, (goal))
+  rank=sorted(fitness, reverse=True)
+  pseudo_fitness=np.zeros_like(fitness)
+  
+  for i in range(k):
+    pseudo_fitness[i] = (k-rank[i])/k
+
+  # order = np.argsort(pseudo_fitness)
+  # pop = pop[order]
+  # Utilizar la ruleta para seleccionar un individuo de la población
+  final_roulette = select_roulette_ranking(pop,pseudo_fitness, k)
+
+  return final_roulette
+
+def temperatura(i,vart,vark):
+  t_c=0.01
+  t_0=vart 
+  k=vark
+  T=t_c+(t_0+t_c)*np.exp(-k*i)
+  return T
+
+def select_boltzmann(pop, mixes, f, k, goal, i,vart,vark):
+  fitness = np.apply_along_axis(f, 1, mixes, (goal))
+  T=temperatura(i,vart,vark)
+  average=np.mean(np.exp(fitness/T))
+  expval=np.zeros_like(fitness)
+  for j in range(len(expval)):
+    expval[j]=(np.exp(fitness[j]/T))/average
+  
+  sum_expval = np.sum(expval)
+  ps = expval / sum_expval
+  qs = np.cumsum(ps)
+  rs = rng.uniform(0., 1., size=(k,))
+
+  selection = []
+  for ri in rs:
+    for m in range(len(qs)):
+      if (qs[m-1] < ri <= qs[m]):
+        selection.append(pop[m])
+
+  return np.array(selection)
+
+# CRUZA
+
+def cross_simple(x, y):
+  s = len(x)
+  p = rng.integers(1, s)
+
+  ch1 = np.concatenate([x[:p], y[p:]])
+  ch2 = np.concatenate([y[:p], x[p:]])
+
+  return ch1, ch2
+
+def cross_double(x, y):
+  s = len(x)
+  p1, p2 = np.sort(rng.integers(0, s, size=2))
+
+  ch1 = np.concatenate([x[:p1], y[p1:p2], x[p2:]])
+  ch2 = np.concatenate([y[:p1], x[p1:p2], y[p2:]])
+
+  return ch1, ch2
+
+def cross_uniform(x, y):
+  ps = rng.uniform(0., 1., size=len(x))
+  
+  ch1 = np.where(ps > 0.5, x, y)
+  ch2 = np.where(ps < 0.5, x, y)
+
+  return ch1, ch2
+
+def cross_n(parents, method):
+  children = []
+  for i in range(0, len(parents) // 2 + 1, 2):
+    ch1, ch2 = method(parents[i], parents[i+1])
+    
+    children.append(ch1)
+    children.append(ch2)
+
+  return np.array(children)
+
+# MUTACION
+def mutate_gen(gen):
+  u = gen * 0.5
+  delta = rng.uniform(-u, u)
+  return gen + delta
+
+def mutate(pi):
+  probs = rng.uniform(0., 1., len(pi))
+  pi = np.where(probs > 0.5, mutate_gen(pi), pi)
+  return pi
+  
+def mutate_n(pop):
+  mutated = np.apply_along_axis(mutate, 1, pop)
+  return mutated
+
+class SelectOption(Enum):
+  ELITE = select_elite
+  ROULETTE = select_roulette
+  TOURNEY = select_tourney
+  BOLTZMANN = select_boltzmann
+
+class CrossOption(Enum):
+  SIMPLE = cross_simple
+  DOUBLE = cross_double
+  UNIFORM = cross_uniform  
--- TP2/utils.py
+++ TP2/utils.py
@@ -31,28 +31,16 @@ def check_finished(iter_amount, iter, pop, mixes, delta, goal, cut_method, selec
   order = np.argsort(aps)
   best = np.flip(mixes[order], axis=0)
   best = best[0]
-
-  xpoints.append(iter)
-  ypoints.append(best_aps)
-  # y_red.append(best[0])
-  # y_green.append(best[1])
-  # y_blue.append(best[2])
-
-  print(best_aps)
   
-  print("Best mix con aptitud = {}".format(best_aps))
-  print(best)
-  print("Props:")
-  pop = np.flip(pop[order], axis=0)
-  print(pop[0])
+
   cut = 0
   if(cut_method == 1):
     if(1-best_aps < delta):
-      return plot_function(xpoints, ypoints, seleccion_method, cross_method, cut_method)
+      return plot_function(order,best, best_aps, goal, pop, seleccion_method, cross_method, cut_method)
     return False
   elif(cut_method == 2):
     if(iter >= iter_amount):
-      return plot_function(xpoints, ypoints, seleccion_method, cross_method, cut_method)
+      return plot_function(order, best, best_aps, goal, pop, seleccion_method, cross_method, cut_method)
     return False
   else:
     if ( iter >= iter_amount or 1 - best_aps < delta):
@@ -60,30 +48,56 @@ def check_finished(iter_amount, iter, pop, mixes, delta, goal, cut_method, selec
         cut = 2
       else:
         cut = 3
-      return plot_function(xpoints, ypoints, seleccion_method, cross_method, cut)
+      return plot_function(order,best, best_aps, goal, pop, seleccion_method, cross_method, cut)
     return False
 
 
-def plot_function(xpoints,ypoints, seleccion_method, cross_method, cut_method):
-  # plt.plot(xpoints, ypoints)
-  # plt.plot(xpoints, y_red, 'r-')
-  # plt.plot(xpoints, y_green, 'g-')
-  # plt.plot(xpoints, y_blue, 'b-')
-  # plt.show()
-
-  # plt.scatter(xpoints, y_red, s=None, c='Red', cmap='Reds')
-  # plt.scatter(xpoints, y_green, s=None, c='Green', cmap='Reds')
-  # plt.scatter(xpoints, y_blue, s=None, c='Blue', cmap='Reds')
-  # plt.xlabel("iteraciones")
-  # plt.ylabel("aptitudes")
-  # plt.title("Selección Elite, Cruza Simple")
-  # plt.show()
-  # print(xpoints)
-  # print(ypoints)
+def plot_function(order,best_color,best_aps, goal, pop, selection_method, cross_method, cut_method):
+  #PRINT OUTPUTS
+  print("Best mix con aptitud = {}".format(best_aps))
+  print(best_color)
+  print("Props:")
+  pop = np.flip(pop[order], axis=0)
+  print(pop[0])
   if(cut_method==1):
     print("A cortado debido a 1-Fitness < DELTA")
   else:
     print("A cortado debido a que se recorrio la totalidad de generaciones")
+    
+  #Show COLORS
+  rgb_values_0 = np.array([best_color]) / 255
+  rgb_values_1 = np.array([goal]) / 255
+  # crear una figura y un eje
+  fig, (ax1, ax2) = plt.subplots(1, 2)
+  # crear una barra de colores usando la matriz de valores RGB
+  ax1.imshow([rgb_values_0])
+  ax2.imshow([rgb_values_1])
+  # configurar el eje para que muestre las etiquetas de los colores 
+  best_color_title = 'Best Color: ' + str(best_color.astype(int))
+  ax1.set_title(best_color_title)
+  ax1.set_yticks([])
+  ax1.set_xticks([])
+  goal_color_title = 'Goal Color: ' + str(goal)
+  ax2.set_title(goal_color_title)
+  ax2.set_yticks([])
+  ax2.set_xticks([])
+
+  selection = "Boltzmann"
+  if(selection_method == 1):
+    selection = "Roulette"
+  elif(selection_method == 2):
+    selection = "Elite"
+  elif(selection_method ==3):
+    selection = "Tourney"
+  cross = "Uniform"
+  if(cross_method == 1):
+    cross = "Simple"
+  elif(cross_method == 2):
+    cross = "Double" 
+  title = "Selección " + selection + ", Cruza " + cross
+  fig.suptitle(str(title))
+  plt.show()
+
   return True
 
 def get_mixes(rgbp):
