5d86106562e42f623db9aa9ff35db918b22d7f9f
unknown <lmannelli@itba.edu.ar>
2023-05-13 00:02:40 -0300

    kohonen heatmap

--- TP4/ej1.py
+++ TP4/ej1.py
@@ -3,45 +3,46 @@ import numpy as np
 import utils
 from constants import FIRST, MIDDLE, LAST
 from kohonen import kohonen_alg
+import matplotlib.pyplot as plt
 import matplotlib
+
 
 def ej1(learning_rate, epochs, type_model, similitud, radio, k):
     input_names, inputs, categories = utils.import_data('data/europe.csv')
     
+    test_idx = 3
     country_name_train = np.array(input_names)
-    # country_name_train = np.delete(country_name_train,3)
+    country_name_train = np.delete(country_name_train,test_idx)
     
     training_set = np.array(inputs, dtype=float)
-    # training_set = np.delete(training_set,3, axis=0)
+    training_set = np.delete(training_set,test_idx, axis=0)
 
     p = len(training_set)
     n = len(training_set[0])
-    iterations = epochs * n
-    if(type_model == 'kohonen'):
-        model = kohonen_alg.Kohonen(p, n, k, radio, learning_rate, similitud, iterations,training_set)
-        neurons_activation = model.train_kohonen()
-        distances = np.zeros(shape=(k, k))
-        for i in range(len(k)):
-            for j in range(len(k)):
-                distances[i][j] = model.get_neighbours_distance(i, j, k)
+    if(type_model == 'kohonen'): 
+        model = kohonen_alg.Kohonen(p, n, k, radio, learning_rate, similitud, epochs,training_set)
+        neurons_weights = model.train_kohonen()
+        # for i in range(k):
+        #     for j in range(k):
+        #         distance = model.get_neighbours_distance(winner_pos, [i,j])
 
         fig, ax = plt.subplots(1, 1)
         cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "orange", "red"])
-        im = ax.imshow(neurons_activation, cmap=cmap)
+        im = ax.imshow(neurons_weights, cmap=cmap)
         fig.colorbar(im)
         plt.title(f'Grilla de neuronas de {k}x{k} ')
         ax.yaxis.set_major_locator(plt.NullLocator())  # remove y axis ticks
         ax.xaxis.set_major_locator(plt.NullLocator())  # remove x axis ticks
         plt.show()
 
-        fig, ax = plt.subplots(1, 1)
-        cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "grey"])
-        im = ax.imshow(distances, cmap=cmap)
-        fig.colorbar(im)
-        plt.title(f'Media de distancia euclidea entre pesos de neuronas vecinas')
-        ax.yaxis.set_major_locator(plt.NullLocator())  # remove y axis ticks
-        ax.xaxis.set_major_locator(plt.NullLocator())  # remove x axis ticks
-        plt.show()
+        # fig, ax = plt.subplots(1, 1)
+        # cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["white", "grey"])
+        # im = ax.imshow(distances, cmap=cmap)
+        # fig.colorbar(im)
+        # plt.title(f'Media de distancia euclidea entre pesos de neuronas vecinas')
+        # ax.yaxis.set_major_locator(plt.NullLocator())  # remove y axis ticks
+        # ax.xaxis.set_major_locator(plt.NullLocator())  # remove x axis ticks
+        # plt.show()
 
     # else:
     #  modelo de oja
--- TP4/kohonen/kohonen_alg.py
+++ TP4/kohonen/kohonen_alg.py
@@ -2,42 +2,24 @@ import numpy as np
 
 class Kohonen:
 
-    def __init__(self,p, n, k,radio, learning_rate, similitud, iterations, X):
+    def __init__(self,p, n, k,radio, learning_rate, similitud, epochs, X):
         self.p = p
         self.n = n
-        # nostras pensamos => k = n => TODO revisar
         self.k = k
-        self.neurons = np.zeros(k,k)
+        self.neurons = np.zeros((k,k))
         self.neurons_reshape = self.neurons.reshape(k**2)
+        self.weights = []
+        # for _ in range(k**2):
+        #     index = np.random.randint(0, p-1)
+        #     x = self.standard_i(X[index])
+        #     self.weights.append(x) 
         self.weights = np.random.rand(k**2,n)
         self.radio = [radio,radio]
         self.learning_rate = [learning_rate,learning_rate]
         self.similitud = similitud
-        self.iterations = iterations
+        self.epochs = epochs
         self.X = X
     
-
-    # def euclidea (self, x,p):
-    #     w=[]
-    #     for j in range(self.k): #recorriendo filas
-    #         w.append(np.abs(x - self.weights[j][p]))
-    #     wk = min(w)
-    #     return w.index(wk)
-    
-    # def exponencial (self, x, p):
-    #     print(self.weights)
-    #     w=[]
-    #     for j in range(self.k): #recorriendo filas
-    #         w.append(np.exp(-((x - self.weights[j][p])**2)))
-    #     wk = min(w)
-    #     return w.index(wk)
-    
-    # def regla_de_kohonen(self, winner_index,x, p):
-    #     # print("REGLA DE KOHENEN")
-    #     for j in range(self.k**2):
-    #         if(np.abs(j-winner_index) <= self.radio[1]):
-    #             # Si soy vecino actulizo mis pesos
-    #             self.weights[j][p] += self.learning_rate[1] * (x-self.weights[j][p])  
         
     def standard(self, X):
         X_standard = []
@@ -50,109 +32,85 @@ class Kohonen:
         std = [np.std(x) for _ in range(len(x))]
         return (x - np.array(mean))/np.array(std)    
     
-    # def winner(self, x,p):
-    #     if(self.similitud == "euclides"):
-    #         return self.euclidea(x,p)
-    #     else:
-    #         return self.exponencial(x,p)
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    def get_distance(neuron1, neuron2):
-        total_sum = 0
-        for i in range(len(neuron1)):
-            total_sum += (neuron1[i] - neuron2[i]) ** 2
-        return np.sqrt(total_sum)
-    
-    def get_neighbours_distance(self, x, y, neurons):
-        curr_neuron_weight = self.neurons[x][y]
-        dist = 0
-        size = 0
-        if y + 1 < len(neurons):
-            dist += self.get_distance(curr_neuron_weight, neurons[x][y + 1].weights)
-            size += 1
-        if y > 0:
-            size += 1
-            dist += self.get_distance(curr_neuron_weight, neurons[x][y - 1].weights)
-        if x > 0:
-            size += 1
-            dist += self.get_distance(curr_neuron_weight, neurons[x - 1][y].weights)
-        if x + 1 < len(neurons):
-            size += 1
-            dist += self.get_distance(curr_neuron_weight, neurons[x + 1][y].weights)
-        return dist / size
+    def get_neighbours_distance(self, winner_pos, neurons):
+        #winner_pos = [x,y]
+        #neurons = [a,b]
+        return np.linalg.norm(winner_pos - neurons)
 
     
-    def regla_de_kohonen_2(self, winner_index,x):
+    def regla_de_kohonen(self, distances, x):
         for j in range(self.k**2):
             # Si soy vecino actulizo mis pesos
-            if(np.abs(j-winner_index) <= self.radio[1]):
+            if(j in distances):
                 for p in range(self.n):
-                    self.weights[j][p] += self.learning_rate[1] * (x-self.weights[j][p])  
+                    self.weights[j][p] += self.learning_rate[1] * (x[p]-self.weights[j][p])  
 
-    def euclidea_2 (self, x):
+    def euclidea(self, x):
         w=[]
         for j in range(self.k**2): #recorriendo filas
-            w.append(np.abs(x - self.weights[j]))
+            w.append(np.linalg.norm(x - self.weights[j]))
         wk = min(w)
         return w.index(wk)
     
-    def exponencial_2 (self, x):
+    def exponencial(self, x):
         w=[]
         for j in range(self.k**2): #recorriendo filas
-            w.append(np.exp(-((x - self.weights[j])**2)))
+            w.append(np.exp(-((np.linalg.norm(x - self.weights[j]))**2)))
         wk = min(w)
         return w.index(wk)
     
-    def winner_2(self, x):
+    def winner(self, x):
         if(self.similitud == "euclides"):
-            return self.euclidea_2(x)
+            return self.euclidea(x)
         else:
-            return self.exponencial_2(x)
+            return self.exponencial(x)
     
-    def activation(self,j):
-        self.neurons_reshape[j]+=1
-        
+    def activation(self,j, epoch):
+        if(epoch == self.epochs - 1):
+            self.neurons_reshape[j]+=1
+            self.neurons = self.neurons_reshape.reshape(self.k,self.k)
+
+        winner_pos = np.unravel_index(j, self.neurons.shape)
+        distances = []
+        # Obtengo la distancia entre neuronas
+        for i in range(self.k):
+            for j in range(self.k):
+                distance = self.get_neighbours_distance(np.array(winner_pos), [i,j])
+                # Veo si son vecinos
+                if(distance <= self.radio[1]):
+                    distances.append(np.ravel_multi_index((i, j), self.neurons.shape))
+        return distances
+    
+    def neurons_weights(self):
+        w = []
+        for j in range(self.k**2):
+            w.append(np.mean(self.weights[j]))
+        return np.reshape(w,(self.k,self.k))
+
+
     def train_kohonen(self):
         X_standard = self.standard(self.X)
         # X_standard = X
-        for i in range(self.iterations):
+        for i in range(self.epochs):
+            print(i)
             for j in range(len(X_standard)):
                 # Seleccionar un registro de entrada X^p
                 x = X_standard[j]
                 # Encontrar la neurona ganadora
-                winner_index = self.winner_2(x)
-                self.activation(winner_index)
+                winner_index = self.winner(x)
+                distances = self.activation(winner_index, i)
                 # Actualizar los pesos segun kohonen
-                self.regla_de_kohonen_2(winner_index, x)
-                # for p in range(len(X_standard[j])):
-                #     # Seleccionar un registro de entrada X^p
-                #     x = X_standard[j][p]
-                #     # Encontrar la neurona ganadora
-                #     winner_index = self.winner(x,p)
-                #     # Actualizar los pesos segun kohonen
-                #     self.regla_de_kohonen(winner_index, x, p)
-                
+                self.regla_de_kohonen(distances, x)
+
             # Ajuste de radio:
-            ajuste = self.radio[0] * (1 - i/self.iterations)
+            ajuste = self.radio[0] * (1 - i/self.epochs)
             self.radio[1] = 1 if ajuste < 1 else ajuste
             # Ajuste de ETA:
-            self.learning_rate[1] = self.learning_rate[0] * (1 - i/self.iterations)
+            self.learning_rate[1] = self.learning_rate[0] * (1 - i/self.epochs)
             # Segunda opcion de ajuste de ETA mas abruta (para ppt probar la dif entre las dos)
             # decay_rate es un hiperparametro que recibiriamos.
             # self.learning_rate[1] = self.learning_rate[0] * np.exp(-decay_rate*i)
 
         self.neurons = self.neurons_reshape.reshape(self.k,self.k)
-        return self.neurons_reshape.reshape(self.k,self.k)
-
-    def test(self, x, country_names, categories):
-        win = []
-        x_standard = self.standard_i(x)
-        for p in range(len(x)):
-            winner_index = self.winner(x_standard[p],p)
-            win.append(winner_index)
-        print("test: ", x)
-        print("categories: ",categories)
-        for i in range(len(categories[0])):
-            print("Categoria: ", categories[0][i])
-            winner = win[i]
-            print("Winner: ", country_names[winner], self.X[winner][i])
-
+        return self.neurons
+        # return self.neurons_weights()
