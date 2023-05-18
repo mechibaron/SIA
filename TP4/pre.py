87c27a645b942fadd8df54e0b7a454bb4e19dcc3
Agustina Ortu <aortu@itba.edu.ar>
2023-05-18 00:14:57 -0300

    PCA completed

--- TP4/pre.py
+++ TP4/pre.py
@@ -3,61 +3,97 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+import utils
+from sklearn import datasets
+import random
 
+def barplot_x(X_standard):
+        X_n = X_standard
+        # area_x=[fila[0] for fila in X]
+        # gdp_x=[fila[1] for fila in X]
+        # inf_x=[fila[2] for fila in X]
+        # life_x=[fila[3] for fila in X]
+        # mil_x=[fila[4] for fila in X]
+        # pop_x=[fila[5] for fila in X]
+        # unem_x=[fila[6] for fila in X]
 
+        area_xn=[fila[0] for fila in X_n]
+        gdp_xn=[fila[1] for fila in X_n]
+        inf_xn=[fila[2] for fila in X_n]
+        life_xn=[fila[3] for fila in X_n]
+        mil_xn=[fila[4] for fila in X_n]
+        pop_xn=[fila[5] for fila in X_n]
+        unem_xn=[fila[6] for fila in X_n]
 
-def get_pca_first_component():
-    return pca.components_[0]
+        # dfx= {'Area': area_x, 'GDP': gdp_x,'Inflation':inf_x,'Life Expect':life_x, 'Military': mil_x, 'Population Growth': pop_x, 'Unemployment': unem_x}
+        # dfx_data=pd.DataFrame(data=dfx, index = None)
+        dfxn= {'Area': area_xn, 'GDP': gdp_xn,'Inflation':inf_xn,'Life Expect':life_xn, 'Military': mil_xn, 'Population Growth': pop_xn, 'Unemployment': unem_xn}
+        dfxn_data=pd.DataFrame(data=dfxn, index = None)
 
-# def principal_components():
-# input_names, inputs, categories = utils.import_data('data/europe.csv')
-data = pd.read_csv('data/europe.csv')
-variables = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
+        # plt.figure(figsize=(25,13))
+        # plt.xlabel('Features',fontsize=15) 
+        # plt.ylabel('Value',fontsize=15)
+        # plt.title(('Non-Standarized Inputs'))
+        # dfx_data.boxplot(column=['Area', 'GDP', 'Inflation','Life Expect','Military','Population Growth','Unemployment'])
+        # plt.show()
 
-#selecciono las columnas especificadas arriba y las aigno a X
-X = data[variables]
+        plt.figure(figsize=(25,13))
+        plt.xlabel('Features',fontsize=15) 
+        plt.ylabel('Value',fontsize=15)
+        plt.title(('Standarized Inputs'))
+        dfxn_data.boxplot(column=['Area', 'GDP', 'Inflation','Life Expect','Military','Population Growth','Unemployment'])
+        plt.show()
 
-#estandarizo los datos
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+        return None
 
+df = pd.read_csv("data/europe.csv", 
+                 names=['Country', 'Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment'], skiprows=[0])
 
-#tengo que hacer el PCA 
-pca = PCA()
-X_pca = pca.fit_transform(X)
+features = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
+# Separating countries
+countries = df['Country'].tolist()
 
-#ahora tengo que sacar las varianzas explicadas
-#explained_variance_ratio: porcentaje de varianza explicado por cada uno de los 
-#componentes seleccionados.
-explained_variances = pca.explained_variance_ratio_
+# Separating out the features
+x = df.loc[:, features].values
 
-loadings = pca.components_[0]
-#Tengo que sacar la carga de variable en la primer componente principal
-# loadings = loadings.T
-loadings = get_pca_first_component()
+# Separating out the target
+y = df.loc[:,['Country']].values
 
-# components_df = pd.DataFrame(data=loadings.reshape(1, -1), columns=variables)
+# Standardizing the features
+x = StandardScaler().fit_transform(x)
+barplot_x(x)
 
+pca = PCA(n_components=2)
 
-# #grafico  boxplot
-# plt.bar(range(1,len(explained_variances)+1), explained_variances)
-# plt.xlabel('Componentes Principales')
-# plt.ylabel('Varianza Explicada')
-# plt.show()
-# components_df = pd.DataFrame(data=loadings, columns=['PC1', 'PC2'])
+principalComponents = pca.fit_transform(x)
 
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
 
-# Graficar el biplot de la primer componente principal
-plt.figure(figsize=(5, 5))
-plt.scatter(X_pca[:, 0], X_pca[:, 1], alpha=0.5)
+finalDf = pd.concat([principalDf, df[['Country']]], axis = 1)
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1) 
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
 
-for i in range(len(variables) - 1):
-    plt.arrow(0, 0, loadings[i] * 3, loadings[i] * 3, head_width=0.01, head_length=0.01, fc='red', ec='red')
-plt.text(loadings[i] * 0.06, loadings[i] * 0.06, variables, color='b', ha='center')
-plt.xlabel('Componente Principal 1')
-plt.ylabel('Componente Principal 2')
-plt.xlim(-0.3, 0.3)
-plt.ylim(-0.3, 0.3)
-plt.grid()
+for i in range(len(countries)):
+    indicesToKeep = finalDf['Country'] == countries[i]
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , s = 50)
+    # ax.text(finalDf.loc[indicesToKeep, 'principal component 1']
+    #            , finalDf.loc[indicesToKeep, 'principal component 2']
+    #            , countries[i])
+    
+# score = principalComponents[:,0:2]
+coeff = np.transpose(pca.components_[0:2, :])
+n = coeff.shape[0]
+for i in range(n):
+    plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
+    plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, features[i], color = 'g', ha = 'center', va = 'center')
+    
+ax.legend(countries)
+ax.grid()
 plt.show()
 
