8b5fe5bda8ec89b9b12c4a53b8c37557b8b98a06
Agustina Ortu <aortu@itba.edu.ar>
2023-05-18 10:21:14 -0300

    End of PCA :)

--- TP4/pca.py
+++ TP4/pca.py
@@ -5,15 +5,15 @@ from sklearn.preprocessing import StandardScaler
 import numpy as np
 import seaborn as sns 
 
-def barplot_x(X_standard):
+def barplot_x(X_standard, X):
         X_n = X_standard
-        # area_x=[fila[0] for fila in X]
-        # gdp_x=[fila[1] for fila in X]
-        # inf_x=[fila[2] for fila in X]
-        # life_x=[fila[3] for fila in X]
-        # mil_x=[fila[4] for fila in X]
-        # pop_x=[fila[5] for fila in X]
-        # unem_x=[fila[6] for fila in X]
+        area_x=[fila[0] for fila in X]
+        gdp_x=[fila[1] for fila in X]
+        inf_x=[fila[2] for fila in X]
+        life_x=[fila[3] for fila in X]
+        mil_x=[fila[4] for fila in X]
+        pop_x=[fila[5] for fila in X]
+        unem_x=[fila[6] for fila in X]
 
         area_xn=[fila[0] for fila in X_n]
         gdp_xn=[fila[1] for fila in X_n]
@@ -23,17 +23,17 @@ def barplot_x(X_standard):
         pop_xn=[fila[5] for fila in X_n]
         unem_xn=[fila[6] for fila in X_n]
 
-        # dfx= {'Area': area_x, 'GDP': gdp_x,'Inflation':inf_x,'Life Expect':life_x, 'Military': mil_x, 'Population Growth': pop_x, 'Unemployment': unem_x}
-        # dfx_data=pd.DataFrame(data=dfx, index = None)
+        dfx= {'Area': area_x, 'GDP': gdp_x,'Inflation':inf_x,'Life Expect':life_x, 'Military': mil_x, 'Population Growth': pop_x, 'Unemployment': unem_x}
+        dfx_data=pd.DataFrame(data=dfx, index = None)
         dfxn= {'Area': area_xn, 'GDP': gdp_xn,'Inflation':inf_xn,'Life Expect':life_xn, 'Military': mil_xn, 'Population Growth': pop_xn, 'Unemployment': unem_xn}
         dfxn_data=pd.DataFrame(data=dfxn, index = None)
 
-        # plt.figure(figsize=(25,13))
-        # plt.xlabel('Features',fontsize=15) 
-        # plt.ylabel('Value',fontsize=15)
-        # plt.title(('Non-Standarized Inputs'))
-        # dfx_data.boxplot(column=['Area', 'GDP', 'Inflation','Life Expect','Military','Population Growth','Unemployment'])
-        # plt.show()
+        plt.figure(figsize=(25,13))
+        plt.xlabel('Features',fontsize=15) 
+        plt.ylabel('Value',fontsize=15)
+        plt.title(('Non-Standarized Inputs'))
+        dfx_data.boxplot(column=['Area', 'GDP', 'Inflation','Life Expect','Military','Population Growth','Unemployment'])
+        plt.show()
 
         plt.figure(figsize=(25,13))
         plt.xlabel('Features',fontsize=15) 
@@ -52,18 +52,18 @@ features = ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth',
 countries = df['Country'].tolist()
 
 # Separating out the features
-x = df.loc[:, features].values
+X = df.loc[:, features].values
 
 # Separating out the target
 y = df.loc[:,['Country']].values
 
 # Standardizing the features
-x = StandardScaler().fit_transform(x)
-barplot_x(x)
+X_standard = StandardScaler().fit_transform(X)
+barplot_x(X_standard, X)
 
 pca = PCA(n_components=2)
 
-principalComponents = pca.fit_transform(x)
+principalComponents = pca.fit_transform(X_standard)
 
 principalDf = pd.DataFrame(data = principalComponents
              , columns = ['principal component 1', 'principal component 2'])
@@ -83,8 +83,7 @@ for i in range(len(countries)):
     # ax.text(finalDf.loc[indicesToKeep, 'principal component 1']
     #            , finalDf.loc[indicesToKeep, 'principal component 2']
     #            , countries[i])
-    
-# score = principalComponents[:,0:2]
+
 coeff = np.transpose(pca.components_[0:2, :])
 n = coeff.shape[0]
 for i in range(n):
@@ -95,10 +94,10 @@ ax.legend(countries)
 ax.grid()
 plt.show()
 
-# explained_variances = pca.explained_variance_ratio_
-
-# plt.bar(range(1,len(explained_variances)+1), explained_variances)
-# plt.xlabel('Componentes Principales')
-# plt.ylabel('Varianza Explicada')
-# plt.show()
-# components_df = pd.DataFrame(data=loadings, columns=['PC1', 'PC2'])
+principal_comp = finalDf['principal component 1'].tolist()
+plt.bar(range(len(countries)), principal_comp)
+plt.title("PCA1 per country")
+plt.xlabel('Countries')
+plt.ylabel('Componentes Principales')
+plt.xticks(np.arange(len(countries)), countries,rotation=45, fontsize=8)
+plt.show()
