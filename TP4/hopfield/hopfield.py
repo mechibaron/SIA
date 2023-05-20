185d4304183106c32ac5612db75c559cb9b83178
unknown <lmannelli@itba.edu.ar>
2023-05-19 22:24:27 -0300

    New ideas for Hopfield

--- TP4/hopfield/hopfield.py
+++ TP4/hopfield/hopfield.py
@@ -12,37 +12,40 @@ class Hopfield:
         self.training_letters = [matriz.flatten() for matriz in training_letters]
         self.p = len(self.training_letters)
         # Weights transpose
-        self.weights = np.matmul(np.transpose(self.training_letters), self.training_letters) / self.n
-        np.fill_diagonal(self.weights, 0)
-
-        self.state_neurons = []
+        self.weights = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                if(i>j):
+                    w_ij = self.sinaptic_weights(i,j)
+                    self.weights[i][j] = w_ij
+                    self.weights[j][i] = w_ij
         self.state_energy = []
+
+    def sinaptic_weights(self, i, j):
+        summatory = 0
+        for mu in range(len(self.training_letters)):
+            summatory+=self.training_letters[mu][i] * self.training_letters[mu][j] 
+        return summatory/self.n
 
     def train(self,noise_letter):
         # Inicializo el estado con el patron de consulta (noise_letter)
-        self.state_neurons = noise_letter.flatten()
-        self.state_energy.append(self.energy())
+        state_neurons = noise_letter.flatten()
+        self.state_energy.append(self.energy(state_neurons))
+        # print("weights: \n", self.weights)
         for e in range(self.epochs):
             print("Iteration: ", e)
             # Si no converge continuo con hopfield
-            converge, idx = self.converge()
-            previous_state = self.state_neurons
-            self.hopfield()
-            response = self.state_neurons.reshape((self.mu, self.mu))
-            # Lo que nos esta pasando es que es estado se actuliza a lo mas cercano a la primera iteracion y no avanza desde ahi
-            # if(converge == True):
-            #     if (np.array_equal(previous_state, self.state_neurons) == True):
-            #         response =  self.matrix_training_letters[idx]
-            #         break
-            #     else: 
-            #         response = self.state_neurons.reshape((self.mu, self.mu))
-            #         break
-            if(np.array_equal(previous_state, self.state_neurons) == True):
+            print(state_neurons)
+            converge, idx = self.converge(state_neurons)
+            previous_state = state_neurons
+            state_neurons = self.hopfield(state_neurons)
+            response = np.array(state_neurons).reshape((self.mu, self.mu))
+
+            if(np.array_equal(previous_state, state_neurons) == True):
                 if (converge == True):
                     response =  self.matrix_training_letters[idx]
                     break
                 else: 
-                    response = self.state_neurons.reshape((self.mu, self.mu))
                     break
 
         
@@ -57,32 +60,35 @@ class Hopfield:
         # Si no encontre estado estable devuelvo false y el ultimo estado alcanzado
         return response
 
-    def hopfield(self):
+    def hopfield(self, state_neurons):
         new_state = []
+
         for i in range(self.n):
-            new_state = self.step_function(np.inner(self.weights[i], self.state_neurons))
-            self.state_neurons[i] = new_state
-        # Try to implement a new idea but nothing changes
-        # self.state_neurons = np.sign(np.dot(self.weights, self.state_neurons))
-        self.state_energy.append(self.energy())
+            print(i)
+            print(self.weights[i])
+            print(state_neurons)
+            new_state.append(self.step_function(np.inner(self.weights[i], state_neurons)))
+        self.state_energy.append(self.energy(new_state))
+        return new_state
         
             
-    def converge(self):
+    def converge(self, state_neurons):
         for l in range(len(self.training_letters)):
-            if (np.array_equal(self.training_letters[l], self.state_neurons) == True):
+            if (np.array_equal(self.training_letters[l], state_neurons) == True):
                 return True, l
         return False, -1
 
-    def energy(self):
+    def energy(self, state_neurons):
         result = 0
         for i in range(self.n):
             for j in range(self.n):
-                result += self.weights[i][j] * self.state_neurons[i] * self.state_neurons[j]
+                result += self.weights[i][j] * state_neurons[i] * state_neurons[j]
         return -0.5 * result
     
     def step_function(self,h):
         if h < 0:
             return -1
+            
         elif h > 0:  
             return 1
         else:
